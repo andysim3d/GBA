fe5af3d773ca26a298fd10853897ee6d530ac6fd
pengfei zhang <pzhang11@stevens.edu>
2015-06-13 12:18:21 -0400

    finish load function.

--- GBAjava/src/hardware/Z80.java
+++ GBAjava/src/hardware/Z80.java
@@ -107,11 +107,430 @@ public class Z80 {
 	 * 
 	 * 
 	 */
-	
-	public void LDrr_bb(){
-		this._r._b = this._r._b;
+	/**
+	 * register B Load
+	 * @param other load from where
+	 */
+	public void LDrr_b(char other){
+		switch(other){
+		case 'b':
+			this._r._b = this._r._b;
+			break;
+		case 'c':
+			this._r._b = this._r._c;
+			break;
+		case 'd':
+			this._r._b = this._r._d;
+			break;
+		case 'e':
+			this._r._b = this._r._e;
+			break;
+		case 'h':
+			this._r._b = this._r._h;
+			break;
+		case 'l':
+			this._r._b = this._r._l;
+			break;
+		case 'a':
+			this._r._b = this._r._a;
+			break;
+		default:
+			break;			
+		}		
 		this._r._m = 1;
 	}
+	public void LDrr_bb(){
+		LDrr_b('b');
+	}
+
+	public void LDrr_bc(){
+		LDrr_b('c');
+	}
+
+	public void LDrr_bd(){
+		LDrr_b('d');
+	}
+
+	public void LDrr_be(){
+		LDrr_b('e');
+	}
+
+	public void LDrr_bh(){
+		LDrr_b('h');
+	}
+
+	public void LDrr_bl(){
+		LDrr_b('l');
+	}
+	public void LDrr_ba(){
+		LDrr_b('a');
+	}
+	
+	
+	public void LDrr_c(char other){
+		switch(other){
+		case 'b':
+			this._r._c = this._r._b;
+			break;
+		case 'c':
+			this._r._c = this._r._c;
+			break;
+		case 'd':
+			this._r._c = this._r._d;
+			break;
+		case 'e':
+			this._r._c = this._r._e;
+			break;
+		case 'h':
+			this._r._c = this._r._h;
+			break;
+		case 'l':
+			this._r._c = this._r._l;
+			break;
+		case 'a':
+			this._r._c = this._r._a;
+			break;
+		default:
+			break;			
+		}		
+		this._r._m = 1;
+	}
+
+	public void LDrr_cb(){
+		LDrr_c('b');
+	}
+
+	public void LDrr_cc(){
+		LDrr_c('c');
+	}
+
+	public void LDrr_cd(){
+		LDrr_c('d');
+	}
+
+	public void LDrr_ce(){
+		LDrr_c('e');
+	}
+
+	public void LDrr_ch(){
+		LDrr_c('h');
+	}
+
+	public void LDrr_cl(){
+		LDrr_c('l');
+	}
+	public void LDrr_ca(){
+		LDrr_c('a');
+	}
+	
+	/**
+	 * Load register d.
+	 */
+
+	public void LDrr_d(char other){
+		switch(other){
+		case 'b':
+			this._r._d = this._r._b;
+			break;
+		case 'c':
+			this._r._d = this._r._c;
+			break;
+		case 'd':
+			this._r._d = this._r._d;
+			break;
+		case 'e':
+			this._r._d = this._r._e;
+			break;
+		case 'h':
+			this._r._d = this._r._h;
+			break;
+		case 'l':
+			this._r._d = this._r._l;
+			break;
+		case 'a':
+			this._r._d = this._r._a;
+			break;
+		default:
+			break;			
+		}		
+		this._r._m = 1;
+	}
+
+	public void LDrr_db(){
+		LDrr_d('b');
+	}
+
+	public void LDrr_dc(){
+		LDrr_d('c');
+	}
+
+	public void LDrr_dd(){
+		LDrr_d('d');
+	}
+
+	public void LDrr_de(){
+		LDrr_d('e');
+	}
+
+	public void LDrr_dh(){
+		LDrr_d('h');
+	}
+
+	public void LDrr_dl(){
+		LDrr_d('l');
+	}
+	public void LDrr_da(){
+		LDrr_d('a');
+	}
+	
+
+	/**
+	 * Load register e.
+	 */
+
+	public void LDrr_e(char other){
+		switch(other){
+		case 'b':
+			this._r._e = this._r._b;
+			break;
+		case 'c':
+			this._r._e = this._r._c;
+			break;
+		case 'd':
+			this._r._e = this._r._d;
+			break;
+		case 'e':
+			this._r._e = this._r._e;
+			break;
+		case 'h':
+			this._r._e = this._r._h;
+			break;
+		case 'l':
+			this._r._e = this._r._l;
+			break;
+		case 'a':
+			this._r._e = this._r._a;
+			break;
+		default:
+			break;			
+		}		
+		this._r._m = 1;
+	}
+
+	public void LDrr_eb(){
+		LDrr_e('b');
+	}
+
+	public void LDrr_ec(){
+		LDrr_e('c');
+	}
+
+	public void LDrr_ed(){
+		LDrr_e('d');
+	}
+
+	public void LDrr_ee(){
+		LDrr_e('e');
+	}
+
+	public void LDrr_eh(){
+		LDrr_e('h');
+	}
+
+	public void LDrr_el(){
+		LDrr_e('l');
+	}
+	public void LDrr_ea(){
+		LDrr_e('a');
+	}
+	
+
+	/**
+	 * Load register h.
+	 */
+
+	public void LDrr_h(char other){
+		switch(other){
+		case 'b':
+			this._r._h = this._r._b;
+			break;
+		case 'c':
+			this._r._h = this._r._c;
+			break;
+		case 'd':
+			this._r._h = this._r._d;
+			break;
+		case 'e':
+			this._r._h = this._r._e;
+			break;
+		case 'h':
+			this._r._h = this._r._h;
+			break;
+		case 'l':
+			this._r._h = this._r._l;
+			break;
+		case 'a':
+			this._r._h = this._r._a;
+			break;
+		default:
+			break;			
+		}		
+		this._r._m = 1;
+	}
+
+	public void LDrr_hb(){
+		LDrr_h('b');
+	}
+
+	public void LDrr_hc(){
+		LDrr_h('c');
+	}
+
+	public void LDrr_hd(){
+		LDrr_h('d');
+	}
+
+	public void LDrr_he(){
+		LDrr_h('e');
+	}
+
+	public void LDrr_hh(){
+		LDrr_h('h');
+	}
+
+	public void LDrr_hl(){
+		LDrr_h('l');
+	}
+	public void LDrr_ha(){
+		LDrr_h('a');
+	}
+	
+	
+
+	/**
+	 * Load register a.
+	 */
+
+	public void LDrr_a(char other){
+		switch(other){
+		case 'b':
+			this._r._a = this._r._b;
+			break;
+		case 'c':
+			this._r._a = this._r._c;
+			break;
+		case 'd':
+			this._r._a = this._r._d;
+			break;
+		case 'e':
+			this._r._a = this._r._e;
+			break;
+		case 'h':
+			this._r._a = this._r._h;
+			break;
+		case 'l':
+			this._r._a = this._r._l;
+			break;
+		case 'a':
+			this._r._a = this._r._a;
+			break;
+		default:
+			break;			
+		}		
+		this._r._m = 1;
+	}
+
+	public void LDrr_ab(){
+		LDrr_a('b');
+	}
+
+	public void LDrr_ac(){
+		LDrr_a('c');
+	}
+
+	public void LDrr_ad(){
+		LDrr_a('d');
+	}
+
+	public void LDrr_ae(){
+		LDrr_a('e');
+	}
+
+	public void LDrr_ah(){
+		LDrr_a('h');
+	}
+
+	public void LDrr_al(){
+		LDrr_a('l');
+	}
+	public void LDrr_aa(){
+		LDrr_a('a');
+	}
+	
+	
+	
+	
+
+	/**
+	 * Load register l.
+	 */
+
+	public void LDrr_l(char other){
+		switch(other){
+		case 'b':
+			this._r._l = this._r._b;
+			break;
+		case 'c':
+			this._r._l = this._r._c;
+			break;
+		case 'd':
+			this._r._l = this._r._d;
+			break;
+		case 'e':
+			this._r._l = this._r._e;
+			break;
+		case 'h':
+			this._r._l = this._r._h;
+			break;
+		case 'l':
+			this._r._l = this._r._l;
+			break;
+		case 'a':
+			this._r._l = this._r._a;
+			break;
+		default:
+			break;			
+		}		
+		this._r._m = 1;
+	}
+
+	public void LDrr_lb(){
+		LDrr_l('b');
+	}
+
+	public void LDrr_lc(){
+		LDrr_l('c');
+	}
+
+	public void LDrr_ld(){
+		LDrr_l('d');
+	}
+
+	public void LDrr_le(){
+		LDrr_l('e');
+	}
+
+	public void LDrr_lh(){
+		LDrr_l('h');
+	}
+
+	public void LDrr_ll(){
+		LDrr_l('l');
+	}
+	public void LDrr_la(){
+		LDrr_l('a');
+	}
+	
 	
 	/**
 	 * Add E to A, leave result in A
