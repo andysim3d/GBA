e118f79b5a86de2ff4a7313c360f9de75bf847a8
Pengfei Zhang <pzhang11@stevens.edu>
2015-06-17 10:38:48 -0400

    reconfig project

--- GBAjava/src/hardware/GPU.java
+++ GBAjava/src/hardware/GPU.java
@@ -1,7 +1,15 @@
 package hardware;
 
 public class GPU {
-	int [] _vram;
-	int [] _oam;
+	int [] _vram = new int[8192];
+	int [] _oam = new int[160];
+	
+	public class Palette{
+		int [] bg = new int[4];
+		int [] obj0 = new int[4];
+		int [] obj1 = new int[4];
+	}
+	
+	public Palette _palete;
 	
 }
